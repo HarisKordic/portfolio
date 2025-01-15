6e58248c710267a1469e4581958a8dd833115322
Haris Kordić <kordicharis18@gmail.com>
2025-01-15 17:37:58 +0100

    fix: update bio

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -2,9 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
-import {
-  AiFillGithub,
-} from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
@@ -14,40 +12,27 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              A BIG <span className="purple"> HELLO </span> FROM ME!
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
+              Welcome to my digital corner of the internet! 🎉<br />
+              I'm a passionate <span className="purple">IT enthusiast</span> who thrives on building cool things with code.
+              With a solid foundation in languages like{" "}
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple">C#, JavaScript, and TypeScript</b>
               </i>
+              , I love diving deep into <span className="purple">Full-Stack development</span>, creating seamless user experiences, and exploring the limitless possibilities of technology. 
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
+              I’m also a fan of <b className="purple">problem-solving</b> and thrive in fast-paced, innovative environments where I can bring my attention to detail and creative mindset to the table.
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              If I’m not coding, you’ll find me learning about the latest trends in{" "}
+              <b className="purple">software engineering</b> or planning my next community project. 🌟
+              <br />
+              <br />
+              Don't forget to check out my{" "}
+              <b className="purple">GitHub portfolio</b> to explore my projects and journey in tech!
             </p>
           </Col>
           <Col md={4} className="myAvtar">
@@ -58,9 +43,9 @@ function Home2() {
         </Row>
         <Row>
           <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
+            <h1>CONNECT WITH ME</h1>
             <p>
-              Feel free to <span className="purple">connect </span>with me
+              Let’s create something <span className="purple">amazing </span> together! 🚀
             </p>
             <ul className="home-about-social-links">
               <li className="social-icons">
